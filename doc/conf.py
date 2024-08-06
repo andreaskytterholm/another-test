95a06324c5b2634f7093f64d7c036b19b4f088a4
Andrea Neverdal Skytterholm <andrea.skytterholm@sintef.no>
2024-08-06 10:07:43 +0200

    test 3434dasdas

--- doc/conf.py
+++ doc/conf.py
@@ -6,23 +6,46 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Example'
-copyright = 'workshop participant'
-author = 'workshop participant'
-release = '0.1'
+import sys; sys.setrecursionlimit(3000)
 
+project = 'SOTA tool'
+copyright = '2024, Andrea Skytterholm and Nektaria Kaloudi'
+author = 'Andrea Skytterholm and Nektaria Kaloudi'
+release = '1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = []
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
+
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+#html_theme = 'alabaster'
+
 html_theme = 'sphinx_rtd_theme'
+
+RTD_NEW_THEME = True
+
+html_theme_options = {
+    'display_version': False,
+}
+
+html_show_sphinx = False
+
 html_static_path = ['_static']
+
+html_css_files = [
+    'nemecys_theme.css',
+]
+
+html_logo = "nemecys_logo.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
--- doc/index.rst
+++ doc/index.rst
@@ -1,14 +1,31 @@
-.. Example documentation master file, created by
-   sphinx-quickstart on Sat Sep 23 20:35:12 2023.
+.. SOTA tool documentation master file, created by
+   sphinx-quickstart on Tue Jun 11 14:23:55 2024.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Example's documentation!
-===================================
+Welcome to the knowledgebase repository
+========================================
+
+..
+   TODO: 
+   - Når en side fjernes, så fjernes ikke html automatisk. Slettet introduction-siden, men html'en slettes ikke når man kjører make html
+   - Når man lenker til en side flere steder kan det bli feil visning i sidemenyen i forhold til hvor du trykket. Mulig løsning: Spesifisere i den aktuelle siden (f.eks. Standard 1 ) at dette dokumentet/denne siden er relevant for både manufacturers og operators
+
+
+
 
 .. toctree::
-   :maxdepth: 2
+   :maxdepth: 1
    :caption: Contents:
 
-   some-feature.md
-   another-feature.md
+   content/manufacturer
+   content/operator
+
+..
+   Indices and tables
+   ==================
+
+   * :ref:`genindex`
+   * :ref:`modindex`
+   * :ref:`search`
+
